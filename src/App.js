aba11aa612f1f394be87535d40c5c1fad01988e9
khazaddoom <31121986g@gmail.com>
2020-05-06 15:02:27 +0530

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Called");
+    return () => {
+      console.log('Cleanup');
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world!!!</h1>
+      <h3>{count}</h3>
+      <button onClick={ () => setCount(count + 1)}>Increase</button>
     </div>
   );
 }
