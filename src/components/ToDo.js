626ec896c12555d42da7a7413c177347301c82dd
khazaddoom <31121986g@gmail.com>
2020-05-08 13:06:50 +0530

    added some styling

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,21 @@
 import React from 'react'
+// import './ToDo.css';
 
 export default function ToDo({todo}) {
 
     return (
         
-        <h3>
-            {todo}
-        </h3>
+            <div className="page">
+                <div className="page__toggle">
+                <label className="toggle">
+                    <input className="toggle__input" type="checkbox" checked />
+                    <span className="toggle__label">
+                        <span className="toggle__text">{todo}</span>
+                    </span>
+                </label>                               
+                </div>
+            </div>
+
+        
     )
 }
